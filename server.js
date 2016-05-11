c723ad6464e2c3fba5d01e0769347c9e0d547c1e
kimsoojin <rlatnwls0611@gmail.com>
2016-05-11 14:01:40 +0900

    remove file before add three.js's editor

--- server.js
+++ server.js
@@ -40,9 +40,9 @@ var server = http.createServer(function(request, response) {
   var filePath = false;
 
   if (request.url == '/') {
-    filePath = 'public/testthreejs.html';
+    filePath = 'ui/index.html';
   } else {
-    filePath = 'public' + request.url;
+    filePath = 'ui' + request.url;
   }
 
   var absPath = './' + filePath;
@@ -53,5 +53,5 @@ server.listen(3000, function() {
   console.log("Server listening on port 3000.");
 });
 
-var chatServer = require('./lib/chat_server');
+var chatServer = require('./ui/js/socket/chat_server');
 chatServer.listen(server);
